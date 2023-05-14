a98d2545475ee16095395e84fb05d31b84b2209f
FREDVUNI <fredvuni809@gmail.com>
2023-05-14 14:19:59 +0300

    set up db and install more dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TodoModule } from './todo/todo.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [
+    TodoModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      port: parseInt(process.env.DB_PORT),
+      host: process.env.DB_HOST,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  dotenv.config();
+
+  //prevents null inputs
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
