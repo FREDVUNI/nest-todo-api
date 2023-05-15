3578bee66d85d004930dd76c60ad09fe1137b9b3
FREDVUNI <fredvuni809@gmail.com>
2023-05-15 12:58:26 +0300

    create dtos

---
+++ src/todo/dtos/todo.create.dto.ts
@@ -0,0 +1,15 @@
+import { IsNotEmpty,IsNumber,IsString,IsBoolean } from 'class-validator';
+
+export class CreateTodoDto{
+    @IsNotEmpty()
+    @IsNumber()
+    id:number;
+
+    @IsNotEmpty()
+    @IsString()
+    item:string;
+
+    @IsNotEmpty()
+    @IsBoolean()
+    completed:boolean;
+}
---
+++ src/todo/dtos/todo.update.dto.ts
@@ -0,0 +1,12 @@
+import { IsNumber,IsString,IsBoolean } from 'class-validator';
+
+export class UpdateTodoDto{
+    @IsNumber()
+    id:number;
+
+    @IsString()
+    item:string;
+
+    @IsBoolean()
+    completed:boolean;
+}
