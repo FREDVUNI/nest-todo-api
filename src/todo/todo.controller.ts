5ab86e7381ae4742ed806d158ac749a05acce167
FREDVUNI <fredvuni809@gmail.com>
2023-05-15 13:10:19 +0300

    create controller

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { TodoService } from './todo.service';
+import { CreateTodoDto } from './dtos/todo.create.dto';
+import { UpdateTodoDto } from './dtos/todo.update.dto';
 
 @Controller('todo')
-export class TodoController {}
+export class TodoController {
+  constructor(private todoService: TodoService) {}
+
+  @Get()
+  async todos() {
+    return this.todoService.getTodos();
+  }
+
+  @Get(':id')
+  async todo(@Param() id: number) {
+    return this.todoService.getTodo(id);
+  }
+
+  @Post()
+  async create(body: CreateTodoDto) {
+    return this.todoService.createTodo(body);
+  }
+
+  @Put(':id')
+  async update(@Param() id: number, body: UpdateTodoDto) {
+    return this.todoService.updateTodo(id, body);
+  }
+
+  @Delete(':id')
+  async delete(@Param() id: number) {
+    return this.todoService.deleteTodo(id);
+  }
+}
