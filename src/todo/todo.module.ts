7fbc0ef9444374ce8438e74bee19cb2ac8c5f2ad
FREDVUNI <fredvuni809@gmail.com>
2023-05-15 12:06:19 +0300

    configure typeorm

--- src/todo/todo.module.ts
+++ src/todo/todo.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TodoService } from './todo.service';
 import { TodoController } from './todo.controller';
+import { Todo } from './entities/todo.entities';
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
   providers: [TodoService],
-  controllers: [TodoController]
+  controllers: [TodoController],
+  imports: [TypeOrmModule.forFeature([Todo])]
 })
-export class TodoModule {}
+export class TodoModule {
+
+}
