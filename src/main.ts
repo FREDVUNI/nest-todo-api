c803ad4c7288b675916d758687a9891404d4d979
FREDVUNI <fredvuni809@gmail.com>
2023-05-19 08:18:07 +0300

    remove unnecessary import

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as dotenv from 'dotenv';
-import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,8 +13,6 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
-
-  app.get(ConfigService);
 
   await app.listen(3000);
 }
